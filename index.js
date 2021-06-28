f0f0cdbb67baa942dd07476dc82f1062b5b74c3b
Hugo Fitas <hfitas@gmail.com>
2021-06-28 18:07:16 +0100

    finished routes for communities

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const connection = require("./config-db");
 
 const port = process.env.PORT;
 
+const authRouter = require("./routes/auth.route");
+const usersRouter = require("./routes/users.route");
+const communityRouter = require("./routes/community.route");
+
 connection.connect((err) => {
   err
     ? console.log(err)
     : console.log(`Connected to database on thread ${connection.threadId}.`);
 });
+
+app.use(express.json());
+
+app.use("/", authRouter);
+app.use("/communities", communityRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, (err) => {
   err ? console.log(err) : console.log(`App is running at port ${port}.`);
