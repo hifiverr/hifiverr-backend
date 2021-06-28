04529526d9a8eaf4accdc3460372dd83b9348c6d
Hugo Fitas <hfitas@gmail.com>
2021-06-28 19:07:49 +0100

    finished events routes

--- routes/events.route.js
+++ routes/events.route.js
@@ -12,12 +12,37 @@ router.get("/:community_id", (req, res) => {
       if (err) res.status(500).json(err);
       else {
         if (eventsByCommunityIdResults.length) {
-          res.status(200).json(eventsByCommunityIdResults[0]);
+          res.status(200).json(eventsByCommunityIdResults);
         } else {
           res.status(404).json({
             errorMessage: `Event with the community id ${communityId} not found.`,
           });
         }
+      }
+    }
+  );
+});
+
+// POST /events
+router.post("/", (req, res) => {
+  const eventInfo = req.body;
+
+  connection.query(
+    "INSERT INTO events SET ?",
+    [eventInfo],
+    (err, eventInsertResult) => {
+      if (err) res.status(500).json(err);
+      else {
+        const eventId = eventInsertResult.insertId;
+
+        connection.query(
+          "SELECT * FROM events WHERE id = ?",
+          [eventId],
+          (err, eventSelectResult) => {
+            if (err) res.status(500).json(err);
+            else res.status(200).json(eventSelectResult[0]);
+          }
+        );
       }
     }
   );
