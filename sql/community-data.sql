8d9df0a61649a03c51a8cff68aba53ddbdb306ec
Filipe Pires <flippires@gmail.com>
2021-06-29 19:00:06 +0100

    community data on sql file

---
+++ sql/community-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO community (name, image) VALUES 
+("Web Development", "https://careerfoundry.com/en/wp-content/uploads/2021/01/web_dev_pillar_page.jpg"),
+("UX / UI", "https://miro.medium.com/max/2000/1*ZrIbCf1pWW6Mtl9TqO2BJQ.png "),
+("Voice Over", "https://s3.eu-central-1.wasabisys.com/courseupload/2020/08/Screen-Shot-2020-08-28-at-4.51.42-PM.png"), 
+("Digital Nomads", "https://i2.wp.com/matheusdesouza.com/wp-content/uploads/2019/06/se-tornar-nomade.jpg?w=960&ssl=1"), 
+("Lisbon Feverr", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Lisbon_Bridge.jpg/1280px-Lisbon_Bridge.jpg"), 
+("Fitness Friends", "https://unsplash.com/photos/PHIgYUGQPvU")
